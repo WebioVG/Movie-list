9feda9f288f9416aca4dc12441bea19a13f2f2d9
webiovg <kevin.kowalsky1@gmail.com>
2022-06-23 17:24:27 +0200

    done: movie details page; to be done: list actors on a details' page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Storage } from '@ionic/storage-angular';
+
 
 @Component({
   selector: 'app-home',
@@ -10,19 +12,30 @@ export class HomePage {
 
   movies: Object[] = []
 
-  constructor(
-    private http: HttpClient
-  ) {}
-
-  // This API comes from www.themoviedb.org/
+    constructor(
+        private http: HttpClient,
+        private storage: Storage
+    ) {}
 
     ionViewWillEnter() {
+
+        // Empty the storage
+        this.storage.create()
+        this.storage.clear()
+        
+        // This API comes from www.themoviedb.org/
         this.http.get('https://api.themoviedb.org/3/movie/popular?api_key=fc7b957c462178c939f7cdf82141cd58')
-        .subscribe(data => {
-            
-            this.movies = data['results']
-            console.log(this.movies[0]);
-        })
+            .subscribe(data => {
+                
+                this.movies = data['results']
+                // console.log(this.movies[0]);
+            }
+        )
+    }
+
+    selectMovie = (movie) => {
+
+        this.storage.set('selectedMovie', JSON.stringify(movie))
     }
 
 }
--- src/app/movie-details/movie-details.page.ts
+++ src/app/movie-details/movie-details.page.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.page.html',
   styleUrls: ['./movie-details.page.scss'],
 })
-export class MovieDetailsPage implements OnInit {
+export class MovieDetailsPage {
 
-  constructor() { }
+  movie: object = {}
 
-  ngOnInit() {
+  constructor(
+    private storage: Storage
+  ) { }
+
+  ionViewWillEnter() {
+    this.storage.get('selectedMovie').then(movie => {
+      this.movie = JSON.parse(movie)
+      console.log(this.movie);
+      
+    })
+    
   }
+
+
 
 }
