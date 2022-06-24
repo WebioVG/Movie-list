a5f7c8d9c7a94d1bca7a1afb67dc713874f3554b
webiovg <kevin.kowalsky1@gmail.com>
2022-06-24 10:56:53 +0200

    done: list actors on a details' page; to be done: actor details page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,33 +4,33 @@ import { Storage } from '@ionic/storage-angular';
 
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  movies: Object[] = []
+    movies: Object[] = []
 
     constructor(
         private http: HttpClient,
         private storage: Storage
-    ) {}
+    ) { }
 
     ionViewWillEnter() {
 
         // Empty the storage
         this.storage.create()
         this.storage.clear()
-        
+
         // This API comes from www.themoviedb.org/
         this.http.get('https://api.themoviedb.org/3/movie/popular?api_key=fc7b957c462178c939f7cdf82141cd58')
             .subscribe(data => {
-                
+
                 this.movies = data['results']
                 // console.log(this.movies[0]);
             }
-        )
+            )
     }
 
     selectMovie = (movie) => {
--- src/app/movie-details/movie-details.page.ts
+++ src/app/movie-details/movie-details.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Storage } from '@ionic/storage-angular';
 
@@ -8,18 +9,30 @@ import { Storage } from '@ionic/storage-angular';
 })
 export class MovieDetailsPage {
 
-  movie: object = {}
+  movie: any = {}
+  actors: any = []
 
   constructor(
-    private storage: Storage
+    private storage: Storage,
+    private http: HttpClient
   ) { }
 
   ionViewWillEnter() {
-    this.storage.get('selectedMovie').then(movie => {
-      this.movie = JSON.parse(movie)
-      console.log(this.movie);
-      
-    })
+    this.storage.get('selectedMovie')
+      .then(movie => {
+        this.movie = JSON.parse(movie)
+        console.log(this.movie);     
+        
+        let movieID = this.movie.id
+        console.log(movieID);
+
+        this.http.get(`https://api.themoviedb.org/3/movie/${this.movie.id}/credits?api_key=fc7b957c462178c939f7cdf82141cd58`)
+          .subscribe((actors: any) => {
+            this.actors = actors.cast
+            console.log(this.actors);
+          })
+        
+      })
     
   }
 
