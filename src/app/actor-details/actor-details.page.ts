3985cc50b92ec5f364246f62858820a2742ab6ba
webiovg <kevin.kowalsky1@gmail.com>
2022-06-24 12:12:25 +0200

    done: actor details page; to be done: link each actor movie to the movie-details page

--- src/app/actor-details/actor-details.page.ts
+++ src/app/actor-details/actor-details.page.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-actor-details',
   templateUrl: './actor-details.page.html',
   styleUrls: ['./actor-details.page.scss'],
 })
-export class ActorDetailsPage implements OnInit {
+export class ActorDetailsPage {
 
-  constructor() { }
+  actor: any = {}
 
-  ngOnInit() {
+  constructor(
+    private storage: Storage,
+    private http: HttpClient
+  ) { }
+
+  ionViewWillEnter() {
+    this.storage.get('selectedActor').then(selectedActorID => {
+      // this.actor = JSON.parse(selectedActorID) 
+      // console.log(typeof selectedActorID);
+      
+      this.http.get(`https://api.themoviedb.org/3/person/${selectedActorID}?api_key=fc7b957c462178c939f7cdf82141cd58&append_to_response=movie_credits`)
+        .subscribe((actor: any) => {
+          this.actor = actor
+          // console.log(this.actor);
+          console.log(this.actor.movie_credits.cast[0]);
+          
+        })
+      
+    })
   }
 
 }
--- src/app/movie-details/movie-details.page.ts
+++ src/app/movie-details/movie-details.page.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Storage } from '@ionic/storage-angular';
 
 @Component({
@@ -21,21 +21,21 @@ export class MovieDetailsPage {
     this.storage.get('selectedMovie')
       .then(movie => {
         this.movie = JSON.parse(movie)
-        console.log(this.movie);     
-        
+        // console.log(this.movie);
+
         let movieID = this.movie.id
-        console.log(movieID);
+        // console.log(movieID);
 
         this.http.get(`https://api.themoviedb.org/3/movie/${this.movie.id}/credits?api_key=fc7b957c462178c939f7cdf82141cd58`)
           .subscribe((actors: any) => {
             this.actors = actors.cast
-            console.log(this.actors);
+            // console.log(this.actors);
           })
-        
       })
-    
   }
 
-
+  selectActor = (id) => {
+    this.storage.set('selectedActor', JSON.stringify(id))    
+  }
 
 }
